2728bd0b1b2d5e7e1da2961c73c93dbd63e86e11
Haeun Rhee <jenhaeun@gmail.com>
2024-09-24 12:40:39 +0900

    fourth commit

--- src/components/More.js
+++ src/components/More.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import './More.css'
+import React from 'react';
+import { getImagePath } from '../utils'; // Import your getImagePath function
+import './More.css';
 
 function More() {
   return (
@@ -7,7 +8,7 @@ function More() {
       <h1 className="appTitle">Explore SEOUL</h1>
 
       <div className='inquiries'>
-        <img className='call' src='./assets/icons/inquiries.png' />
+        <img className='call' src={getImagePath('assets/icons/inquiries.png')} alt="Tourism Inquiries" />
         <p>Tourism Inquiries <span>1330</span> </p>
       </div>
 
@@ -16,50 +17,50 @@ function More() {
       <div className='moreMenuContainer'>
         <div className='moreMenu'>
           <div className='moreMenuImage'>
-            <img className='moreMenuIcon' src='./assets/icons/preparing.png' />
+            <img className='moreMenuIcon' src={getImagePath('assets/icons/preparing.png')} alt="Preparing for the Trip" />
           </div>
           <p className='moreMenuText'>Preparing for the Trip</p>
         </div>
         <div className='moreMenu'>
           <div className='moreMenuImage'>
-            <img className='moreMenuIcon' src='./assets/icons/useful_phrases.png' />
+            <img className='moreMenuIcon' src={getImagePath('assets/icons/useful_phrases.png')} alt="Useful Phrases" />
           </div>
           <p className='moreMenuText'>Useful Phrases</p>
         </div>
         <div className='moreMenu'>
           <div className='moreMenuImage'>
-            <img className='moreMenuIcon' src='./assets/icons/ebook.png' />
+            <img className='moreMenuIcon' src={getImagePath('assets/icons/ebook.png')} alt="Maps & Guidebooks" />
           </div>  
           <p className='moreMenuText'>Maps & Guidebooks</p>
         </div>
         <div className='moreMenu'>
           <div className='moreMenuImage'>
-            <img className='moreMenuIcon' src='./assets/icons/shopping.png' />
+            <img className='moreMenuIcon' src={getImagePath('assets/icons/shopping.png')} alt="Shopping List" />
           </div>
           <p className='moreMenuText'>Shopping List</p>
         </div>
         <div className='moreMenu'>
           <div className='moreMenuImage'>
-            <img className='moreMenuIcon' src='./assets/icons/tax.png' />
+            <img className='moreMenuIcon' src={getImagePath('assets/icons/tax.png')} alt="Tax Refunds" />
           </div>
           <p className='moreMenuText'>Tax Refunds</p>
         </div>
         <div className='moreMenu'>
           <div className='moreMenuImage'>
-            <img className='moreMenuIcon' src='./assets/icons/medical.png' />
+            <img className='moreMenuIcon' src={getImagePath('assets/icons/medical.png')} alt="Medical Tourism" />
           </div>
           <p className='moreMenuText'>Medical Tourism</p>
         </div>
         <div className='moreMenu'>
           <div className='moreMenuImage'>
-            <img className='moreMenuIcon' src='./assets/icons/resources.png' />
+            <img className='moreMenuIcon' src={getImagePath('assets/icons/resources.png')} alt="Helpful Apps & Resources" />
           </div>
           <p className='moreMenuText'>Helpful Apps & Resources</p>
         </div>
       </div>
 
     </div>
-  )
+  );
 }
 
-export default More
+export default More;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 // src/components/NavBar.js
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { getImagePath } from '../utils'; // Import your getImagePath function
 import './NavBar.css'; // Import your CSS file
 
 function NavBar() {
@@ -14,7 +15,7 @@ function NavBar() {
     <div className="navBarContainer">
       <Link to="/" className={`navItem ${isActive('/') ? 'active' : ''}`}>
         <img
-          src={isActive('/') ? '/assets/icons/home_selected.png' : '/assets/icons/home.png'}
+          src={isActive('/') ? getImagePath('assets/icons/home_selected.png') : getImagePath('assets/icons/home.png')}
           alt="Home"
           className="navIcon1"
         />
@@ -22,7 +23,7 @@ function NavBar() {
       </Link>
       <Link to="/map" className={`navItem ${isActive('/map') ? 'active' : ''}`}>
         <img
-          src={isActive('/map') ? '/assets/icons/map_selected.png' : '/assets/icons/map.png'}
+          src={isActive('/map') ? getImagePath('assets/icons/map_selected.png') : getImagePath('assets/icons/map.png')}
           alt="Map"
           className="navIcon2"
         />
@@ -30,7 +31,7 @@ function NavBar() {
       </Link>
       <Link to="/travel" className={`navItem ${isActive('/travel') ? 'active' : ''}`}>
         <img
-          src={isActive('/travel') ? '/assets/icons/travel_selected.png' : '/assets/icons/travel.png'}
+          src={isActive('/travel') ? getImagePath('assets/icons/travel_selected.png') : getImagePath('assets/icons/travel.png')}
           alt="Travel"
           className="navIcon1"
         />
@@ -38,7 +39,7 @@ function NavBar() {
       </Link>
       <Link to="/more" className={`navItem ${isActive('/more') ? 'active' : ''}`}>
         <img
-          src={isActive('/more') ? '/assets/icons/more_selected.png' : '/assets/icons/more.png'}
+          src={isActive('/more') ? getImagePath('assets/icons/more_selected.png') : getImagePath('assets/icons/more.png')}
           alt="More"
           className="navIcon3"
         />
